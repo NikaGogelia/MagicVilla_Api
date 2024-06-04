4cd363faff937ffb6dc3e42ce2f7643b617f09f3
NikaGogelia <gogelianika1@gmail.com>
2024-06-04 16:40:09 +0400

    update

--- Controllers/VillaApiController.cs
+++ Controllers/VillaApiController.cs
@@ -4,6 +4,7 @@ using MagicVilla_Api.Models;
 using MagicVilla_Api.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_Api.Controllers;
 
@@ -12,19 +13,21 @@ namespace MagicVilla_Api.Controllers;
 [ApiController]
 public class VillaApiController : ControllerBase
 {
+	private readonly ApplicationDbContext _db;
 	private readonly ILogging _logger;
 
-	public VillaApiController(ILogging logger)
+	public VillaApiController(ApplicationDbContext context, ILogging logger)
 	{
+		_db = context;
 		_logger = logger;
 	}
 
 	[HttpGet]
 	[ProducesResponseType(StatusCodes.Status200OK)]
-	public ActionResult<IEnumerable<VillaDto>> GetVillas()
+	public async Task<ActionResult<IEnumerable<VillaDto>>> GetVillas()
 	{
 		_logger.Log("Getting all villas", "");
-		return Ok(VillaStore.villaList);
+		return Ok(await _db.Villas.ToListAsync());
 	}
 
 	[HttpGet("{id:int}", Name = "GetVilla")]
@@ -32,7 +35,7 @@ public class VillaApiController : ControllerBase
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	//[ProducesResponseType(200, Type = typeof(VillaDto))]
-	public ActionResult<VillaDto> GetVilla(int id)
+	public async Task<ActionResult<VillaDto>> GetVilla(int id)
 	{
 		if (id == 0)
 		{
@@ -40,7 +43,7 @@ public class VillaApiController : ControllerBase
 			return BadRequest();
 		}
 
-		var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+		var villa = await _db.Villas.FirstOrDefaultAsync(u => u.Id == id);
 
 		if (villa == null)
 		{
@@ -54,14 +57,9 @@ public class VillaApiController : ControllerBase
 	[ProducesResponseType(StatusCodes.Status201Created)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-	public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villa)
+	public async Task<ActionResult<VillaDto>> CreateVilla([FromBody] VillaCreateDto villa)
 	{
-		//if (!ModelState.IsValid)
-		//{
-		//	return BadRequest(ModelState);
-		//}
-
-		if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villa.Name.ToLower()) != null)
+		if (await _db.Villas.FirstOrDefaultAsync(u => u.Name.ToLower() == villa.Name.ToLower()) != null)
 		{
 			ModelState.AddModelError("CustomError", "Villa already exists!");
 			return BadRequest(ModelState);
@@ -72,54 +70,71 @@ public class VillaApiController : ControllerBase
 			return BadRequest(villa);
 		}
 
-		if (villa.Id > 0)
+		Villa model = new()
 		{
-			return StatusCode(StatusCodes.Status500InternalServerError);
-		}
+			Amenity = villa.Amenity,
+			Details = villa.Details,
+			ImageUrl = villa.ImageUrl,
+			Name = villa.Name,
+			Occupancy = villa.Occupancy,
+			Rate = villa.Rate,
+			Sqft = villa.Sqft,
+		};
 
-		villa.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
-		VillaStore.villaList.Add(villa);
+		await _db.Villas.AddAsync(model);
+		await _db.SaveChangesAsync();
 
-		return CreatedAtRoute("GetVilla", new { id = villa.Id }, villa);
+		return CreatedAtRoute("GetVilla", new { id = model.Id }, model);
 	}
 
 	[HttpDelete("{id:int}", Name = "DeleteVilla")]
 	[ProducesResponseType(StatusCodes.Status204NoContent)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
-	public IActionResult DeleteVilla(int id)
+	public async Task<IActionResult> DeleteVilla(int id)
 	{
 		if (id == 0)
 		{
 			return BadRequest();
 		}
 
-		var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+		var villa = await _db.Villas.FirstOrDefaultAsync(u => u.Id == id);
 
 		if (villa == null)
 		{
 			return NotFound();
 		}
 
-		VillaStore.villaList.Remove(villa);
+		_db.Villas.Remove(villa);
+		await _db.SaveChangesAsync();
+
 		return NoContent();
 	}
 
 	[HttpPut("{id:int}", Name = "UpdateVilla")]
 	[ProducesResponseType(StatusCodes.Status204NoContent)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
-	public IActionResult UpdateVilla(int id, [FromBody] VillaDto villaDto)
+	public async Task<IActionResult> UpdateVilla(int id, [FromBody] VillaUpdateDto villaDto)
 	{
 		if (villaDto == null || id != villaDto.Id)
 		{
 			return BadRequest();
 		}
 
-		var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+		Villa model = new()
+		{
+			Amenity = villaDto.Amenity,
+			Details = villaDto.Details,
+			Id = villaDto.Id,
+			ImageUrl = villaDto.ImageUrl,
+			Name = villaDto.Name,
+			Occupancy = villaDto.Occupancy,
+			Rate = villaDto.Rate,
+			Sqft = villaDto.Sqft,
+		};
 
-		villa.Name = villaDto.Name;
-		villa.Sqft = villaDto.Sqft;
-		villa.Occupancy = villaDto.Occupancy;
+		_db.Villas.Update(model);
+		await _db.SaveChangesAsync();
 
 		return NoContent();
 	}
@@ -127,21 +142,48 @@ public class VillaApiController : ControllerBase
 	[HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
 	[ProducesResponseType(StatusCodes.Status204NoContent)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
-	public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDto> patchDto)
+	public async Task<IActionResult> UpdatePartialVilla(int id, JsonPatchDocument<VillaUpdateDto> patchDto)
 	{
 		if (patchDto == null || id == 0)
 		{
 			return BadRequest();
 		}
 
-		var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+		var villa = await _db.Villas.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
+
+		VillaUpdateDto villaDto = new()
+		{
+			Amenity = villa.Amenity,
+			Details = villa.Details,
+			Id = villa.Id,
+			ImageUrl = villa.ImageUrl,
+			Name = villa.Name,
+			Occupancy = villa.Occupancy,
+			Rate = villa.Rate,
+			Sqft = villa.Sqft,
+		};
 
 		if (villa == null)
 		{
 			return BadRequest();
 		}
 
-		patchDto.ApplyTo(villa, ModelState);
+		patchDto.ApplyTo(villaDto, ModelState);
+
+		Villa model = new()
+		{
+			Amenity = villaDto.Amenity,
+			Details = villaDto.Details,
+			Id = villaDto.Id,
+			ImageUrl = villaDto.ImageUrl,
+			Name = villaDto.Name,
+			Occupancy = villaDto.Occupancy,
+			Rate = villaDto.Rate,
+			Sqft = villaDto.Sqft,
+		};
+
+		_db.Villas.Update(model);
+		await _db.SaveChangesAsync();
 
 		if (!ModelState.IsValid)
 		{
---
+++ Migrations/20240603161725_addCreatedDateInformationVillaTable.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCreatedDateInformationVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 20, 17, 25, 291, DateTimeKind.Local).AddTicks(4308));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 20, 17, 25, 291, DateTimeKind.Local).AddTicks(4320));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 20, 17, 25, 291, DateTimeKind.Local).AddTicks(4321));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 20, 17, 25, 291, DateTimeKind.Local).AddTicks(4323));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 3, 20, 17, 25, 291, DateTimeKind.Local).AddTicks(4324));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
